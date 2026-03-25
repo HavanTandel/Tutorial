d080118cbbbf1c096c502e081733f8d7a3f70810
HavankumarTandel <tandelhavu@gmail.com>
2026-03-25 10:31:01 +0530

    Change VITE_API_URL code

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getStoredToken } from "./auth";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://a99740ee92415435f9abe6c44d7a620b-1171611830.ap-south-1.elb.amazonaws.com";
-// const API_BASE_URL = import.meta.env.VITE_API_URL
+// const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
